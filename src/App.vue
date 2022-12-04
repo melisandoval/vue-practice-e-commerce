04d68c29c319019f3cdb20262daeef884c8d9c1c
melisand <melisandoval@live.com.ar>
2022-12-04 21:45:51 +0100

    create vue project and add navbar

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <header class="w-full bg-[#27272a] text-white relative">
+    <nav class="py-8 z-0">
+      <ul class="w-full inline-flex justify-center">
+        <li><a class="px-8" href="#">AllProducts</a></li>
+        <li><a class="px-8" href="#">Smartphones</a></li>
+        <li><a class="px-8" href="#">Notebooks</a></li>
+      </ul>
+    </nav>
+    <button class="absolute z-10 top-0 right-0 pr-16 pt-8">
+      <span>Cart</span>
+      <img src="" alt="" />
+    </button>
+  </header>
+</template>
+
+<style scoped></style>

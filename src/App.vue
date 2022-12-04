83d036b59c2faf35e98e88970304a25ada1f09c5
melisand <melisandoval@live.com.ar>
2022-12-05 00:59:07 +0100

    add single product card

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <script setup></script>
 
 <template>
-  <header class="w-full bg-[#27272a] text-white relative">
-    <nav class="py-8 z-0">
-      <ul class="w-full inline-flex justify-center">
-        <li><a class="px-8" href="#">AllProducts</a></li>
-        <li><a class="px-8" href="#">Smartphones</a></li>
-        <li><a class="px-8" href="#">Notebooks</a></li>
-      </ul>
-    </nav>
-    <button class="absolute z-10 top-0 right-0 pr-16 pt-8">
-      <span>Cart</span>
-      <img src="" alt="" />
-    </button>
-  </header>
+  <body class="bg-slate-50">
+    <header class="w-full bg-[#27272a] text-white relative">
+      <nav class="py-8 z-0">
+        <ul class="w-full inline-flex justify-center">
+          <li><a class="px-8" href="#">AllProducts</a></li>
+          <li><a class="px-8" href="#">Smartphones</a></li>
+          <li><a class="px-8" href="#">Notebooks</a></li>
+        </ul>
+      </nav>
+      <button class="absolute z-10 top-0 right-0 pr-16 pt-8">
+        <span>Cart</span>
+        <img src="" alt="" />
+      </button>
+    </header>
+
+    <main>
+      <section>
+        <!-- single card -->
+        <a href="">
+          <div
+            class="relative flex-col justify-items-center bg-white p-4 m-8 max-w-[250px]"
+          >
+            <img src="https://via.placeholder.com/250x250" alt="" />
+            <h2 class="py-4 text-lg">
+              Notebook Lenovo Ideapad 320 Intel® Core i5-7200u 8GB
+            </h2>
+            <p class="text-end">$ 2259, 00</p>
+            <button class="w-full bg-green-500 p-4 my-4 text-white">
+              Add to cart
+            </button>
+          </div>
+        </a>
+        <!-- end of single card -->
+      </section>
+    </main>
+  </body>
 </template>
 
 <style scoped></style>

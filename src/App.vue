ecb14fd810fe19dd6f241b0f288128eeccf4739b
melisand <melisandoval@live.com.ar>
2022-12-05 01:28:52 +0100

    add cart symbol

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,10 @@
         </ul>
       </nav>
       <button
-        class="absolute bg-sky-500 rounded-md z-10 top-0 right-0 my-6 mx-8 py-2 px-8"
+        class="absolute flex align-center bg-sky-500 rounded-md z-10 top-0 right-0 my-6 mx-8 py-2 px-8"
       >
-        <span>Cart</span>
+        <span class="px-2">Cart</span>
+        <span class="material-symbols-outlined"> shopping_cart </span>
         <img src="" alt="" />
       </button>
     </header>
